5f696abe6108150e3c245a4e8c8ab50da27f2896
Your Name <pskwong@outlook.com>
2025-01-06 13:09:30 +0000

    Dependencies Installed * Error Handler

--- app.js
+++ app.js
@@ -2,9 +2,11 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const errorhandler = require("errorhandler");
 
 
 // ---------------------- Apply Modules -----------
 const app = express();
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
+app.use(errorhandler());
