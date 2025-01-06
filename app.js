69ee3d0880729140d964478bb012c208adef5db2
Your Name <pskwong@outlook.com>
2025-01-06 13:14:54 +0000

    Dependencies Installed * CORS

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const errorhandler = require("errorhandler");
+const cors = require('cors'); 
 
+// ---------------------- Global Variable -----------
+const port = process.env.PORT || 3008;
 
 // ---------------------- Apply Modules -----------
 const app = express();
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 app.use(errorhandler());
+app.use(cors()); 
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
