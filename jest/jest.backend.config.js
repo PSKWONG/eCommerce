79d7452babc539578d9f3378e952676b6fe5eedb
Edward <pskwong@outlook.com>
2025-01-10 12:09:03 +0000

    BackEnd Test Configuration file updated

--- jest/jest.backend.config.js
+++ jest/jest.backend.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    globalSetup: './jest.setup.js',
-    globalTeardown: './jest.teardown.js',
-    testEnvironment: 'node',
-    testMatch: ['<rootDir>/backendTests/**/*.test.js'], // Match all test files in the "tests" directory
-  };
+  globalSetup: './jest.setup.js',
+  globalTeardown: './jest.teardown.js',
+  testEnvironment: 'node',
+  testRegex: '(/jest/backendTests/.*|(\\.|/)(test|spec))\\.js$', // Match all test files in the "backendTests" directory
+};
