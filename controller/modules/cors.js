26bd6b08d821931a8c20cf1c46f637015e999aa3
Edward <pskwong@outlook.com>
2025-01-14 23:27:57 +0000

    Update on CORS

--- controller/modules/cors.js
+++ controller/modules/cors.js
@@ -7,9 +7,13 @@ const environment = process.env.NODE_ENV;
 const productionServer = process.env.PRO_FRONTEND_SERVER_URL;
 const developmentServer = process.env.DEV_FRONTEND_SERVER_URL;
 
+
+
 const corsConfiguration = {
     origin: environment === 'production' ? productionServer : developmentServer, // Allow the frontend to send requests to the backend
     credentials : true // Allow cookies to be sent and received
 }
+
+console.log('corsConfiguration', corsConfiguration);
 
 module.exports = cors(corsConfiguration);
