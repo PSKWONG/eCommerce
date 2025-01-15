271cce4d07e431c2735b03a7edc45a96ac80ff2d
Edward <pskwong@outlook.com>
2025-01-15 12:50:59 +0000

    Hook Migration Completed

--- view/src/containers/registration/RegContainer.js
+++ view/src/containers/registration/RegContainer.js
@@ -1,13 +1,8 @@
 // ------------------------ Import Modules --------------------------
-import React, { useEffect } from 'react';
-import { checkUserInput, submitRegistration } from '../../features/api/API';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
 
 // ------------------------ Import Components --------------------------
 import RegPage from '../../components/registration/Registration';
-import styles from '../../components/registration/registration.module.css';
-import { selectIsAuthenticated, checkAuth } from "../../features/authentication/authenticationSlice";
 
 // Form States 
 import useFormStates from './states';
@@ -15,6 +10,8 @@ import useFormStates from './states';
 import useFormActions from './formHandlers';
 //Form Effect Hook 
 import useFormEffect from './effectHook';
+//Form Data Constructor Hook 
+import useFormData from './formData';
 
 
 
@@ -24,82 +21,17 @@ const RegContainer = () => {
     //States in registration forms 
     const formStates = useFormStates();
 
-    const {
-        username, setUsername,
-        email, setEmail,
-        password, setPassword,
-        confirmPassword, setConfirmPassword,
-        isValidUsername, setIsValidUsername,
-        isValidEmail, setIsValidEmail,
-        isValidPassword, setIsValidPassword,
-        isValidConfirmPassword, setIsValidConfirmPassword,
-        isFormCompleted, setIsFormCompleted,
-        guideline, setGuideline
-    } = formStates;
-
-    //Form Actions 
-    const { handleOnChange, handleValidation, handleSubmission } = useFormActions(formStates);
+    //Form Handlers 
+    const formHandlers = useFormActions(formStates);
 
     //Effect Hook
     useFormEffect(formStates);
 
-
-
-    //Registration form Object Constructor 
-    const regFormData = {
-        registrationForm:
-        {
-            //Data for the registration form input fields
-            data: [
-                {
-                    name: 'username',
-                    title: 'Username:',
-                    type: 'text',
-                    value: username,
-                    error: isValidUsername === false ? styles.error : styles.valid,
-                },
-                {
-                    name: 'email',
-                    title: 'Email:',
-                    type: 'email',
-                    value: email,
-                    error: isValidEmail === false ? styles.error : styles.valid,
-                },
-                {
-                    name: 'password',
-                    title: 'Password:',
-                    type: 'password',
-                    value: password,
-                    error: isValidPassword === false ? styles.error : styles.valid,
-                },
-                {
-                    name: 'confirmPassword',
-                    title: 'Confirm Password:',
-                    type: 'password',
-                    value: confirmPassword,
-                    error: isValidConfirmPassword === false ? styles.error : styles.valid,
-                }
-            ],
-            //Data for the submit button
-            submit: {
-                style: isFormCompleted ? styles.submitButton : styles.submitButtonDisabled,
-            },
-            //Data for the actions
-            actions: {
-                handleOnChange,
-                handleValidation,
-                handleSubmission
-            }
-        }
-        ,
-        guidelines: {
-            //Error message for the registration form
-            data: guideline
-        }
-    }
-
+    //Form Data Constructor Hook 
+    const formData = useFormData(formStates, formHandlers);
+   
     return (
-        < RegPage {...regFormData} />
+        < RegPage {...formData} />
     );
 };
 
