96b0119f2e762acbbd0a8be83cf9eefc1b69f3b3
Edward <pskwong@outlook.com>
2025-01-20 17:48:54 +0000

    Implementation of Product Detail Components

--- view/src/components/product/productDetail/ProductDetail.js
+++ view/src/components/product/productDetail/ProductDetail.js
@@ -1,17 +1,46 @@
 // -------------------------- Import Modules --------------------------
 import React from 'react';
 
+
 //-------------------------- Import Components --------------------------
+import '../../../assets/styles/App.css';
+import styles from './ProductDetail.module.css';
+//envrioment
+const environment = process.env.REACT_APP_ENV;
+//API URL
+const server_URL = environment === 'production' ? process.env.REACT_APP_API_URL_PROD : process.env.REACT_APP_API_URL_DEV;
 
 
 
 
 
 //-------------------------- ProductDetail Component --------------------------
-const ProductDetail = () => {
+const ProductDetail = ({ productDetail, countData }) => {
+    const { product_name, description, image_path, unit_price } = productDetail;
+    const imageBaseURL = server_URL + '/assets/productImage/';
     return (
-        <div>
-            ProductDetail
+        <div className={`PageWrapper`}> {/* A div element */}
+            <div className={` floatContentWrapper twoColumnWrapper ${styles.productDetailWrapper}`}>
+                <div className={` leftColumn ${styles.contentWrapper} ${styles.productWrapper}`}>
+                    <h1>{product_name} </h1>
+                    <div className={styles.productDetailWrapper}>
+                        <img src={imageBaseURL + image_path} alt={product_name} className={styles.contentWrapper}/>
+                        <div className={`${styles.contentWrapper} ${styles.descriptionDisplay}`}>
+                            <p>{description}</p>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div className={` rightColumn ${styles.contentWrapper} ${styles.actionWrapper} `}>
+                    <div className={styles.priceTag}>
+                        Price: <em>{unit_price}</em>
+                    </div>
+                    <div>
+                    </div>
+                    
+
+                </div>
+            </div>
         </div>
     );
 };
